3ad875288ea5d7c016a4fd1cdb65d841fe0d1d40
Berke Ikikarakayali <berke.karakaya35@outlook.com>
2025-12-18 23:02:18 +0300

    Pause Menu Implementation

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    [Header("UI References")]
+
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(settingsMenuUI.activeSelf)
+            {
+
+                CloseSettings();
+
+            } else if (GameIsPaused)
+            {
+            
+            Resume();
+
+            } else
+            {
+
+            Pause();
+
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);    
+        settingsMenuUI.SetActive(false); 
+        Time.timeScale = 1f; //restore time      
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);     
+        Time.timeScale = 0f;             
+        GameIsPaused = true;
+    }
+
+
+    //settings menu
+    public void OpenSettings()
+    {
+        pauseMenuUI.SetActive(false);   
+        settingsMenuUI.SetActive(true); 
+    }
+
+    // back to the pause menu
+    public void CloseSettings()
+    {
+        settingsMenuUI.SetActive(false); 
+        pauseMenuUI.SetActive(true);     
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; //restore time
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
